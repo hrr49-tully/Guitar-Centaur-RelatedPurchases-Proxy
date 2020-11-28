2484425f14bc283f3da20f98df9fd1b5464fdc3b
Joe Wnukoski <59515951+jwnukoski@users.noreply.github.com>
2020-11-28 11:18:23 -0500

    styling (#7)
    * ec2 proxy for service 1
    
    * service 2
    
    * add-to-cart module added
    
    * renamed services
    
    * service 2
    
    * fixed styles
    
    * style width
    
    * page width style
    
    * hacky stylesheet for modules
    
    * service 4
    
    * service 4
    
    * table-cell fix for style conflict
    
    * hacky styles
    
    * more hacky styles for module 2

--- server/config/services.js
+++ server/config/services.js
@@ -42,23 +42,23 @@
 
 module.exports = {
   service1: {
-    api: '/related',
-    url: 'http://localhost:3001',
+    api: ['/items', '/photos'],
+    url: 'http://54.214.116.240:3000',
     bundle: 'bundle.js',
   },
   service2: {
-    api: ['/items', '/photos'],
-    url: 'http://localhost:3002',
+    api: ['/values', '/styles'],
+    url: 'http://107.22.130.204:3003',
     bundle: 'bundle.js',
   },
   service3: {
-    api: ['/values', '/styles'],
-    url: 'http://localhost:3003',
+    api: '/related',
+    url: 'http://ec2-18-219-22-8.us-east-2.compute.amazonaws.com:3003',
     bundle: 'bundle.js',
   },
   service4: {
     api: ['/:endpoint', '/guitar/reviews'],
-    url: 'http://localhost:3004',
+    url: 'http://ec2-18-219-89-193.us-east-2.compute.amazonaws.com:3004',
     bundle: 'bundle.js',
   },
 };
